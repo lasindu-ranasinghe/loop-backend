6c79de15cde55c1bd35037a57bbfd68c02e453a6
lasindu-ranasinghe <116148700+lasindu-ranasinghe@users.noreply.github.com>
2024-02-18 15:53:01 +0530

    minor changes

--- index.js
+++ index.js
@@ -1,18 +1,3 @@
-// *****************************************************
-//                   CARAMEL BACKEND
-// *****************************************************
-// This is the 'driver code' (commonly referred to as
-// the 'entry point') of the backend of the Caramel App.
-// This is a NodeJS application built using the ExpressJS
-// framework.
-//
-// To run this application, open up a terminal in this
-// directory, and enter the following commands:
-// 1. npm i
-// 2. node .
-// *****************************************************
-
-// IMPORTS AND OTHER REQUIREMENTS
 const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/database");
@@ -58,49 +43,9 @@ app.use("/email", email);
 app.use("/messege", messegeRoutes);
 app.use("/payment", paymentRoutes);
 
-// DATABASE CONNECTION
-
-// The Caramel App is powered by a MongoDB database hosted on an
-// Atlas server. The 'database.js' module in the config directory
-// (as imported above) is responsible for orchestrating the
-// entire database connection process, including any error handling.
-// The driver code (this file) will simply call the connectDB()
-// method in the 'database.js' module.
-
-// connecting to the database
-connectDB();
-
-// SERVER SETUP
-
-// 1. Starting up the server
-
-// This is a simple console log to indicate that 'index.js'
-// is being run.
-
 console.log(`BACKEND SERVER STARTED IN ${config.get("Name")} ENVIRONMENT`);
 
-// 2. Setting up the port number for the server to listen to
-
-// Here, the server will listen to the port number specified
-// in the PORT variable (as is the case with hosting services
-// such as Heroku). If this variable is not pre-defined (e.g.
-// we are running this app locally), the server will default to
-// listening to port number 3001:
-
 const port = process.env.PORT;
-
-// NOTE:
-// Port 3001 has been used specifically since we cannot use NodeJS's
-// default port 3000, since the NextJS app (the frontend) uses
-// that port when we are running this project locally.
-
-// 3. Starting listening to requests on previously specified port
-
-// The NextJS frontend will send HTTP requests to the backend through
-// port number 3001, which the backend must start listening to.
-// The listen() function of the 'app' instance also includes a callback
-// function to log 'Listening to Port <port-number>' to the console
-// after listening begins:
 
 connectDB().then(() => {
   app.listen(port, () => {
